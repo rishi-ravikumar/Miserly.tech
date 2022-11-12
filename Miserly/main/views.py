d32301d73e1fb41830497cc40dac8021118dd353
vishalsekar04 <vishalsekar04@gmail.com>
2022-11-12 23:35:25 +0000

    Added url paths and logic in views

--- Miserly/main/views.py
+++ Miserly/main/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+	# return render(request, "homepage.html")
+	# if request.method=="POST":
+		return search(request)
+
+def search(request):
+	return render(request, "sample.html", {
+		'table_info' : [{"name": "milk", "supermarkets":[["ASDA", "£2"], ["Sainsbury's", "£3"], ["Tesco", "£4"]]},
+		{"name": "eggs", "supermarkets":[["ASDA", "£1"], ["Sainsbury's", "£2"], ["Tesco", "£3"]]}]
+		})
