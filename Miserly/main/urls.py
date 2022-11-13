88473d08a8e2afa5c23dc895f0efe92e58dd6836
vishalsekar04 <vishalsekar04@gmail.com>
2022-11-13 01:23:06 +0000

    Added the homepage template

--- Miserly/main/urls.py
+++ Miserly/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.search, name='search'),
+	path("", views.index, name='index'),
 ]
 
--- Miserly/main/views.py
+++ Miserly/main/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	# return render(request, "homepage.html")
+	# return render(request, "index.html")
 	# if request.method=="POST":
 		return search(request)
 
 def search(request):
 	return render(request, "sample.html", {
-		'table_info' : [{"name": "milk", "supermarkets":[["ASDA", "£2"], ["Sainsbury's", "£3"], ["Tesco", "£4"]]},
-		{"name": "eggs", "supermarkets":[["ASDA", "£1"], ["Sainsbury's", "£2"], ["Tesco", "£3"]]}]
+		'table_info' : [{"search_name" : "milk", "results" : [{"item_name": "Cravendale", "item_des": "Fat Milk", "supermarkets":[["ASDA", "£2"], ["Sainsbury's", "£3"], ["Tesco", "£4"]]},
+		{"item_name": "Asla", "item_des": "Fat Milk", "supermarkets":[["ASDA", "£2.5"], ["Sainsbury's", "£3.5"], ["Tesco", "£4.5"]]}]},
+
+		{"search_name" : "eggs", "results" : [{"item_name": "Cravendale", "item_des": "12 eggs", "supermarkets":[["ASDA", "£4"], ["Sainsbury's", "£5"], ["Tesco", "£3"]]},
+		{"item_name": "Cravendale", "item_des": "6 eggs", "supermarkets":[["ASDA", "£2"], ["Sainsbury's", "£2"], ["Tesco", "£1"]]}]}
+		]
 		})
